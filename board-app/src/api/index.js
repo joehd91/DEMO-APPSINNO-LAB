08285b17e30e7eaf62ce8b9a84e063e8c21f78ae
Patrick Jang <patrick_jang@PATRICK-DESKTOP.localdomain>
2022-10-22 12:15:55 +0900

    appsinno commit

--- board-app/src/api/index.js
+++ board-app/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: 'https://appsinnodnslabel.koreacentral.cloudapp.azure.com:8080',
+	baseURL: 'https://appsinnodnslabel.koreacentral.cloudapp.azure.com/',
 })
 
 instance.defaults.headers.post['Content-Type'] =
